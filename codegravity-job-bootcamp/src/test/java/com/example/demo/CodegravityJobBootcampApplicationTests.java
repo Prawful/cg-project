f90362742fd5e4de8d4d528d04258f26a06e6a46
Prafulla Maharjan <prafulla.maharjan@snhu.edu>
2024-03-20 14:15:21 -0400

    test

---
+++ codegravity-job-bootcamp/src/test/java/com/example/demo/CodegravityJobBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegravityJobBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
